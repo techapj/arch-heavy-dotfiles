25748fbac18f73166111c1554e7afcfc60e6d82d
devlin zed <me@devlinzed.com>
2013-08-27 14:51:58 -0400

    unused code?

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -5,45 +5,14 @@ autoload colors
 colors
 
 autoload -U add-zsh-hook
-autoload -Uz vcs_info
-
-turquoise="$fg[cyan]"
-orange="$fg[yellow]"
-purple="$fg[magenta]"
-hotpink="$fg[red]"
-limegreen="$fg[green]"
 
 function prompt_char {
-    [[ -n "$vim_mode" ]] && echo "$vim_mode" && return
-    git branch >/dev/null 2>/dev/null && echo '±' && return
-    echo '$'
+  if [[ -n "$vim_mode" ]]; then
+    echo "$vim_mode"
+  else
+    echo "$"
+  fi
 }
-
-# enable VCS systems you use
-zstyle ':vcs_info:*' enable git
-
-# check-for-changes can be really slow.
-# you should disable it, if you work with large repositories
-zstyle ':vcs_info:*:prompt:*' check-for-changes true
-
-# set formats
-# %b - branchname
-# %u - unstagedstr (see below)
-# %c - stagedstr (see below)
-# %a - action (e.g. rebase-i)
-# %R - repository path
-# %S - path in the repository
-PR_RST="%{${reset_color}%}"
-FMT_BRANCH="(%{$turquoise%}%b%u%c${PR_RST})"
-FMT_ACTION="(%{$limegreen%}%a${PR_RST})"
-FMT_UNSTAGED="%{$orange%}●"
-FMT_STAGED="%{$limegreen%}●"
-
-zstyle ':vcs_info:*:prompt:*' unstagedstr "${FMT_UNSTAGED}"
-zstyle ':vcs_info:*:prompt:*' stagedstr "${FMT_STAGED}"
-zstyle ':vcs_info:*:prompt:*' actionformats "${FMT_BRANCH}${FMT_ACTION}"
-zstyle ':vcs_info:*:prompt:*' formats "${FMT_BRANCH}"
-zstyle ':vcs_info:*:prompt:*' nvcsformats ""
 
 PROMPT='
 %{$fg[yellow]%}${PWD/#$HOME/~}%{$reset_color%}$(git_prompt_info)
@@ -60,24 +29,23 @@ vim_ins_mode=""
 vim_cmd_mode="!"
 vim_mode=$vim_ins_mode
 
-function zle-keymap-select {
+zle-keymap-select() {
   vim_mode="${${KEYMAP/vicmd/${vim_cmd_mode}}/(main|viins)/${vim_ins_mode}}"
   zle reset-prompt
 }
 zle -N zle-keymap-select
 
-function zle-line-finish {
+zle-line-finish() {
   vim_mode=$vim_ins_mode
 }
 zle -N zle-line-finish
 
-# get the name of the branch we are on
-function git_prompt_info() {
+git_prompt_info() {
   ref=$(git symbolic-ref HEAD 2> /dev/null) || return
   echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
 }
 
-parse_git_dirty () {
+parse_git_dirty() {
   gitstat=$(git status 2>/dev/null | grep '\(# Untracked\|# Changes\|# Changed but not updated:\)')
 
   if [[ $(echo ${gitstat} | grep -c "^\(# Changes to be committed:\|# Changes not staged for commit:\)$") > 0 ]]; then
@@ -95,13 +63,4 @@ parse_git_dirty () {
   if [[ -d ".git/trusted" ]]; then
     echo -n "$ZSH_THEME_GIT_PROMPT_TRUSTED"
   fi
-}
-
-#
-# Will return the current branch name
-# Usage example: git pull origin $(current_branch)
-#
-function current_branch() {
-  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
-  echo ${ref#refs/heads/}
 }

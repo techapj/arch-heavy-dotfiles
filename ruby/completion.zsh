160000053a28fe8cfdffed6445f2b39aa3e1d785
devlin zed <me@devlinzed.com>
2013-09-17 12:54:54 -0400

    rake completion

--- ruby/completion.zsh
+++ ruby/completion.zsh
@@ -1,22 +1,38 @@
-# Stolen from
-#   https://github.com/sstephenson/rbenv/blob/master/completions/rbenv.zsh
-
-if [[ ! -o interactive ]] || [[ ! -d "$HOME/.rbenv" ]]; then
-  return
-fi
-
-compctl -K _rbenv rbenv
-
-_rbenv() {
-  local word words completions
-  read -cA words
-  word="${words[2]}"
-
-  if [ "${#words}" -eq 2 ]; then
-    completions="$(rbenv commands)"
+_rake_does_task_list_need_generating () {
+  if [[ ! -f .rake_tasks ]]; then
+    return 0
   else
-    completions="$(rbenv completions "${word}")"
+    accurate="$(stat -f%m .rake_tasks)"
+    changed="$(stat -f%m Rakefile)"
+    return "$(expr $accurate '>=' $changed)"
   fi
-
-  reply=("${(ps:\n:)completions}")
 }
+
+_rake () {
+  if [[ -f "Rakefile" ]]; then
+    if _rake_does_task_list_need_generating; then
+      rake --silent --tasks | cut -d " " -f 2 > ."rake_tasks"
+    fi
+    compadd `cat .rake_tasks`
+  fi
+}
+
+compdef _rake rake
+
+if [[ -o interactive ]] && [[ -d "$HOME/.rbenv" ]]; then
+  compctl -K _rbenv rbenv
+
+  _rbenv() {
+    local word words completions
+    read -cA words
+    word="${words[2]}"
+
+    if [ "${#words}" -eq 2 ]; then
+      completions="$(rbenv commands)"
+    else
+      completions="$(rbenv completions "${word}")"
+    fi
+
+    reply=("${(ps:\n:)completions}")
+  }
+fi

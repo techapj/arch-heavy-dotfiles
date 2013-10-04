de65bc598e580e0704ff700319d975fb819ae7b7
devlin zed <me@devlinzed.com>
2013-10-04 10:00:15 -0400

    less tags

--- awesome/awesome.symlink/rc.lua
+++ awesome/awesome.symlink/rc.lua
@@ -84,7 +84,7 @@ end
 tags = {}
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
-    tags[s] = awful.tag({ " ", " ", " ", " ", " ", " ", }, s, layouts[1])
+    tags[s] = awful.tag({ " ", " ", " ", " " }, s, layouts[1])
 end
 -- }}}
 

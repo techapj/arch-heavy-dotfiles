3fc205da23971ee856c8c8d124c23cc09bff5035
devlin zed <me@devlinzed.com>
2013-10-14 11:40:17 -0400

    scrolling to change tags is pretty irritating with the wallpaper visible

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -189,8 +189,8 @@ end
 
 -- {{{ Mouse bindings
 root.buttons(awful.util.table.join(
-    awful.button({ }, 4, awful.tag.viewnext),
-    awful.button({ }, 5, awful.tag.viewprev)
+    -- awful.button({ }, 4, awful.tag.viewnext),
+    -- awful.button({ }, 5, awful.tag.viewprev)
 ))
 -- }}}
 

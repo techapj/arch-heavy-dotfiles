f54814745f91e696c4e51802f99074576a02accf
devlin zed <me@devlinzed.com>
2013-10-22 12:07:17 -0400

    eventually move the git prompt info to tmux

--- zsh/zshrc.d/60-prompt.zsh
+++ zsh/zshrc.d/60-prompt.zsh
@@ -2,35 +2,7 @@ PR_GIT_UPDATE=1
 
 setopt prompt_subst
 autoload colors; colors
-autoload -U add-zsh-hook
-
-git_prompt_info() {
-  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
-  printf "%s" "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
-}
-
-parse_git_dirty() {
-  gitstat=$(git status 2>/dev/null | grep '\(# Untracked\|# Changes\|# Changed but not updated:\)')
-
-  if [[ $(printf "%s" ${gitstat} | grep -c "^\(# Changes to be committed:\|# Changes not staged for commit:\)$") > 0 ]]; then
-    printf "%s" "$ZSH_THEME_GIT_PROMPT_DIRTY"
-  fi
-
-  if [[ $(printf "%s" ${gitstat} | grep -c "^\(# Untracked files:\|# Changed but not updated:\)$") > 0 ]]; then
-    printf "%s" "$ZSH_THEME_GIT_PROMPT_UNTRACKED"
-  fi
-
-  if [[ -d ".git/trusted" ]]; then
-    printf "%s" "$ZSH_THEME_GIT_PROMPT_TRUSTED"
-  fi
-}
 
 PROMPT='
-%{$fg[yellow]%}${PWD/#$HOME/~}%{$reset_color%}$(git_prompt_info)%{$fg[red]%}%{$reset_color%}
+%{$fg[yellow]%}${PWD/#$HOME/~}%{$reset_color%}
 $ '
-
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[green]%}!"
-ZSH_THEME_GIT_PROMPT_PREFIX=" on %{$fg[magenta]%}"
-ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
-ZSH_THEME_GIT_PROMPT_TRUSTED="%{$fg[red]%}^"
-ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[green]%}?"

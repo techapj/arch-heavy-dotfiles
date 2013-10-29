fe1777cd08a72c894a05cfd608a51f884cf29b1f
devlin zed <me@devlinzed.com>
2013-10-29 15:05:00 -0400

    less quotes! yay!

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,4 @@
-set -gx DOTFILES "$HOME/.dotfiles"
+set -gx DOTFILES $HOME/.dotfiles
 set -gx PATH .git/trusted/../../bin $DOTFILES/bin $HOME/.local/bin /usr/local/bin /usr/bin
 
 set -gx EDITOR vim
@@ -25,7 +25,7 @@ set fish_color_user green
 function update_tmux --description "Send the current directory to tmux." --on-event fish_prompt
   if set -q TMUX
     set -l var_name (tmux display -p "TMUX_PWD_#D")
-    tmux setenv "$var_name" "$PWD"
+    tmux setenv $var_name $PWD
   end
 end
 
@@ -42,6 +42,6 @@ function fish_prompt --description "Minimal prompt & update tmux."
   set_color normal
 end
 
-if test -f "$HOME/.localrc.fish"
-  . "$HOME/.localrc.fish"
+if test -f $HOME/.localrc.fish
+  . $HOME/.localrc.fish
 end

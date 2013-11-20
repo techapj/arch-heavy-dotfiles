238d10cd5f8a28b4917b874d84b918e06b6da08b
devlin zed <me@devlinzed.com>
2013-11-20 11:41:30 -0500

    different song format

--- awesome/bashets/mpc.zsh
+++ awesome/bashets/mpc.zsh
@@ -5,11 +5,14 @@ output="$(mpc)"
 state="$(sed -n 2p <<< "$output" | cut -d \  -f 1)"
 
 if [[ "$state" = "[playing]" ]]; then
-  string="%s"
+  string="%s by %s"
 elif [[ "$state" = "[paused]" ]]; then
-  string="Paused: %s"
+  string="[Paused]%s by %s" # first %s has a leading space
 fi
 
 if [[ -n "$string" ]]; then
-  printf "$string" "$(head -n 1 <<< "$output")"
+  raw="$(head -n 1 <<< "$output")"
+  song="$(cut -d - -f 2- <<< "$raw")"
+  artist="$(cut -d - -f 1 <<< "$raw")"
+  printf "$string" "$song" "$artist"
 fi

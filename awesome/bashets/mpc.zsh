5c5c95382d8e4f0fa88f5124a9e3057e67d07010
devlin zed <me@devlinzed.com>
2013-12-06 15:10:42 -0500

    now thats efficiency

--- awesome/bashets/mpc.zsh
+++ awesome/bashets/mpc.zsh
@@ -7,9 +7,9 @@ state="$(sed -n 2p <<< "$output" | cut -d \  -f 1)"
 if [[ "$state" = "[playing]" ]]; then
   string="%s"
 elif [[ "$state" = "[paused]" ]]; then
-  string="[Paused] %s" # first %s has a leading space
+  string="Paused: %s"
 fi
 
 if [[ -n "$string" ]]; then
-  printf "$string" "$(head -n 1 <<< "$output" | sed "s# - #/#")"
+  sed "s# - #/#;1q;d" <<< "$output"
 fi

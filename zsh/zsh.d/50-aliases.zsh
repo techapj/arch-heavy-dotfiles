0c83bb967422cd63ee7678f668372ebd7cb6b01f
devlin zed <me@devlinzed.com>
2013-12-18 13:25:16 -0500

    move aliases to functions

--- zsh/zsh.d/50-aliases.zsh
+++ zsh/zsh.d/50-aliases.zsh
@@ -1,16 +1,2 @@
 alias "sudo"="sudo " # allow sudo with aliases
-
-alias "1st"="awk '{ print \$1 }'"
-alias "2nd"="awk '{ print \$2 }'"
-alias "3rd"="awk '{ print \$3 }'"
-
-for i in 4 5 6 7 8 9; do
-  alias "${i}th"="awk '{ print \$$i }'"
-done
-unset i
-
-alias "rspec"="nocorrect rspec"
-
 alias "pacman"="powerpill"
-
-alias "git"="hub"
